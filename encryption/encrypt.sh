8ccc8357a38355f087bfd4a77427d3d84b4ad5f6
haru <haru@inl.ics.keio.ac.jp>
2018-06-30 01:20:00 +0900

    Support directory encryption

--- encryption/encrypt.sh
+++ encryption/encrypt.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-openssl aes-256-cbc -e -in $1 -out $1.enc -pass file:$HOME/.PASSWORD
+path=$1
+if [ -f $path ]; then
+  openssl aes-256-cbc -e -in $path -out $path.enc -pass file:$HOME/.PASSWORD
+  rm $path
+elif [ -d $path ]; then
+  tar czf $path.tar.gz $path
+  tgz_path=$path.tar.gz
+  openssl aes-256-cbc -e -in $tgz_path -out $tgz_path.enc -pass file:$HOME/.PASSWORD
+  rm -rf $path
+  rm $tgz_path
+else
+  echo "Error: $path is not a file or directory"
+fi
