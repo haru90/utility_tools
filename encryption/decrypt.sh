b5ecb01833906a12fa72668fbaf926734a1ef9e4
haru <haru@inl.ics.keio.ac.jp>
2018-07-03 16:14:34 +0900

    Update decrypt.sh

--- encryption/decrypt.sh
+++ encryption/decrypt.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-openssl aes-256-cbc -d -in $1 -out ${1%.enc} -pass file:$HOME/.PASSWORD
+path=$1
+openssl aes-256-cbc -d -in $path -out ${path%.enc} -pass file:$HOME/.PASSWORD
+rm $path
+if [[ $path =~ ^.+\.tar\.gz\.enc$ ]]; then
+  tar xzf ${path%.enc}
+  rm ${path%.enc}
+fi
