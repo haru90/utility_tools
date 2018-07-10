b054576659d846f1687df61a2811a76840916f3a
haru <haru@inl.ics.keio.ac.jp>
2018-07-10 10:21:32 +0900

    Update flac_to_mp3.py

--- flac_to_mp3/flac_to_mp3.py
+++ flac_to_mp3/flac_to_mp3.py
@@ -3,35 +3,40 @@ import os
 import subprocess
 import sys
 
+
 def encode(in_dir_path, out_root_dir_path):
     for f in glob.glob(os.path.join(in_dir_path, '*')):
         if os.path.isdir(f):
             out_dir_path = os.path.join(out_root_dir_path, os.path.basename(f))
             if not os.path.exists(out_dir_path):
                 os.mkdir(out_dir_path)
-            print('f: ' + f, '\nout_dir_path: ', out_dir_path)
             encode(f, out_dir_path)
 
     for in_path in glob.glob(os.path.join(in_dir_path, '*.flac')):
         in_basename = os.path.splitext(os.path.basename(in_path))[0]
         out_path = os.path.join(out_root_dir_path, in_basename + '.mp3')
-        ffmpeg_args = ['ffmpeg', '-i', in_path, '-ab', '320k', '-c:v', 'copy', out_path]
+        ffmpeg_argv = ['ffmpeg', '-i', in_path, '-ab', '320k', '-c:v', 'copy', '-n', out_path]
         try:
-            print('Encoding ' + in_path)
-            subprocess.run(ffmpeg_args)
+            print('\nEncoding ' + in_path + '\n')
+            subprocess.run(ffmpeg_argv)
         except:
-            print("Error: " + in_path)
+            print("Error: Fail to encode " + in_path)
 
 
-if len(sys.argv) != 3:
-    print('Usage: python flac_to_mp3.py [input directory path] [output directory path]')
-    sys.exit(1)
+def main(argv):
+    if len(argv) != 3:
+        print('Usage: python flac_to_mp3.py [input directory path] [output directory path]')
+        sys.exit(1)
 
-in_dir_path = sys.argv[1]
-out_root_dir_path = sys.argv[2]
+    in_dir_path = argv[1]
+    out_root_dir_path = argv[2]
 
-out_dir_path = os.path.join(out_root_dir_path, os.path.basename(in_dir_path))
-if not os.path.exists(out_dir_path):
-    os.mkdir(out_dir_path)
+    out_dir_path = os.path.join(out_root_dir_path, os.path.basename(in_dir_path))
+    if not os.path.exists(out_dir_path):
+        os.mkdir(out_dir_path)
 
-encode(in_dir_path, out_dir_path)
+    encode(in_dir_path, out_dir_path)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
