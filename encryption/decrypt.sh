e042c0c8743a8ba4fa414b09fd6c217e61a7bd9e
haru <haru@inl.ics.keio.ac.jp>
2018-09-10 23:41:13 +0900

    Delete old encryption shell scripts

--- encryption/decrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path=$1
-openssl aes-256-cbc -d -in $path -out ${path%.enc} -pass file:$HOME/.PASSWORD
-rm $path
-if [[ $path =~ ^.+\.tar\.gz\.enc$ ]]; then
-  tar xzf ${path%.enc}
-  rm ${path%.enc}
-fi
