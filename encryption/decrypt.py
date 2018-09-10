3710c573ac1ad460e0808207159dde460eda8d8c
haru <haru@inl.ics.keio.ac.jp>
2018-09-10 23:40:05 +0900

    Add encryption Python scripts

---
+++ encryption/decrypt.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import click
+import os
+import re
+import subprocess
+import sys
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+PASSWORD_F_PATH = '{}/.PASSWORD'.format(os.environ['HOME'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.argument('input_path')
+def decrypt(input_path):
+    out_path = os.path.splitext(input_path)[0]
+    try:
+        result = subprocess.run(['openssl', 'aes-256-cbc', '-d', '-in', input_path, '-out', out_path, '-pass',
+                                 'file:{}'.format(PASSWORD_F_PATH)])
+        if result.returncode != 0:
+            sys.exit('Error: Failed to decrypt {}'.format(input_path))
+        subprocess.run(['rm', input_path])
+        if re.match(r'^.+\.tar\.gz$', out_path):
+            result = subprocess.run(['tar', 'xzf', out_path])
+            if result.returncode != 0:
+                sys.exit('Error: Failed to extract {}'.format(out_path))
+            subprocess.run(['rm', out_path])
+    except Exception as e:
+        sys.exit(e)
+
+
+if __name__ == '__main__':
+    decrypt()
---
+++ encryption/encrypt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import click
+import os
+import subprocess
+import sys
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+PASSWORD_F_PATH = '{}/.PASSWORD'.format(os.environ['HOME'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.argument('input_path')
+def encrypt(input_path):
+    try:
+        if os.path.isfile(input_path):
+            result = subprocess.run(
+                ['openssl', 'aes-256-cbc', '-e', '-in', input_path, '-out', '{}.enc'.format(input_path), '-pass',
+                 'file:{}'.format(PASSWORD_F_PATH)])
+            if result.returncode != 0:
+                sys.exit('Error: Failed to encrypt {}'.format(input_path))
+            subprocess.run(['rm', input_path])
+        elif os.path.isdir(input_path):
+            tgz_path = '{}.tar.gz'.format(input_path)
+            subprocess.run(['tar', 'czf', tgz_path, input_path])
+            result = subprocess.run(
+                ['openssl', 'aes-256-cbc', '-e', '-in', tgz_path, '-out', '{}.enc'.format(tgz_path), '-pass',
+                 'file:{}'.format(PASSWORD_F_PATH)])
+            if result.returncode != 0:
+                print('Error: Failed to encrypt {}'.format(input_path), file=sys.stderr)
+                result = subprocess.run(['tar', 'xzf', tgz_path])
+                if result.returncode == 0:
+                    subprocess.run(['rm', tgz_path])
+                    sys.exit(1)
+                else:
+                    sys.exit('Error: Failed to extract {}'.format(tgz_path))
+            subprocess.run(['rm', '-rf', input_path])
+            subprocess.run(['rm', tgz_path])
+        else:
+            sys.exit('Error: {} is not a file or directory'.format(input_path))
+    except Exception as e:
+        sys.exit(e)
+
+
+if __name__ == '__main__':
+    encrypt()
